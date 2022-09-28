bc8a57e8c3688dd408c72db02918ce4b890c51ea
JBarreraBallestas <jbarreraballestas@correo.unicordoba.edu.co>
2022-09-28 16:59:39 -0500

    Add scores

--- triqui.js
+++ triqui.js
@@ -6,12 +6,10 @@ var level;
 let last;
 let m;
 
-console.log([
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-]);
-
+let wins = localStorage.wins ? localStorage.wins : 0;
+let loses = localStorage.loses ? localStorage.loses : 0;
+let draws = localStorage.draws ? localStorage.draws : 0;
+check();
 function play(pos, i) {
   if (!finish) {
     last = pos;
@@ -26,13 +24,13 @@ function play(pos, i) {
 
 function playL1() {
   mcount++;
-  if (aval[4]==null) {
+  if (aval[4] == null) {
     return 4;
-  } 
+  }
   if (m == undefined) {
     for (let index = 0; index < aval.length; index++) {
       const element = aval[index];
-      if (element == 'o' && aval[index + 1] == null && (index+1)<aval.length) {
+      if (element == 'o' && aval[index + 1] == null && (index + 1) < aval.length) {
         return index + 1;
       }
     }
@@ -46,43 +44,43 @@ function playL1() {
       return pl1;
     }
   }
-  
+
   return Math.floor(Math.random() * 9);
 }
 
 function blockCols() {
-  c1 = [aval[0],aval[3],aval[6]];
-  c2 = [aval[1],aval[4],aval[7]];
-  c3 = [aval[2],aval[5],aval[8]];
-  if (c1.filter(x => x=='o').length == 2 && c1.filter(x => x=='x').length == 0) {
-    console.log(c1.indexOf(null)*3);
-    return c1.indexOf(null)*3;
+  c1 = [aval[0], aval[3], aval[6]];
+  c2 = [aval[1], aval[4], aval[7]];
+  c3 = [aval[2], aval[5], aval[8]];
+  if (c1.filter(x => x == 'o').length == 2 && c1.filter(x => x == 'x').length == 0) {
+    console.log(c1.indexOf(null) * 3);
+    return c1.indexOf(null) * 3;
   }
-  if (c2.filter(x => x=='o').length == 2 && c2.filter(x => x=='x').length == 0) {
-    console.log((c2.indexOf(null)*3)+1);
-    return (c2.indexOf(null)*3)+1;
+  if (c2.filter(x => x == 'o').length == 2 && c2.filter(x => x == 'x').length == 0) {
+    console.log((c2.indexOf(null) * 3) + 1);
+    return (c2.indexOf(null) * 3) + 1;
   }
-  if (c3.filter(x => x=='o').length == 2 && c3.filter(x => x=='x').length == 0) {
-    console.log((c3.indexOf(null)*3)+2);
-    return (c3.indexOf(null)*3)+2;
+  if (c3.filter(x => x == 'o').length == 2 && c3.filter(x => x == 'x').length == 0) {
+    console.log((c3.indexOf(null) * 3) + 2);
+    return (c3.indexOf(null) * 3) + 2;
   }
   return undefined;
 }
 function blockRows() {
-  r1 = [aval[0],aval[1],aval[2]];
-  r2 = [aval[3],aval[4],aval[5]];
-  r3 = [aval[6],aval[7],aval[8]];
-  if (r1.filter(x => x=='o').length == 2 && r1.filter(x => x=='x').length == 0) {
+  r1 = [aval[0], aval[1], aval[2]];
+  r2 = [aval[3], aval[4], aval[5]];
+  r3 = [aval[6], aval[7], aval[8]];
+  if (r1.filter(x => x == 'o').length == 2 && r1.filter(x => x == 'x').length == 0) {
     console.log(r1.indexOf(null));
     return r1.indexOf(null);
   }
-  if (r2.filter(x => x=='o').length == 2 && r2.filter(x => x=='x').length == 0) {
-    console.log(r2.indexOf(null)+3);
-    return r2.indexOf(null)+3;
+  if (r2.filter(x => x == 'o').length == 2 && r2.filter(x => x == 'x').length == 0) {
+    console.log(r2.indexOf(null) + 3);
+    return r2.indexOf(null) + 3;
   }
-  if (r3.filter(x => x=='o').length == 2 && r3.filter(x => x=='x').length == 0) {
-    console.log(r3.indexOf(null)+6);
-    return r3.indexOf(null)+6;
+  if (r3.filter(x => x == 'o').length == 2 && r3.filter(x => x == 'x').length == 0) {
+    console.log(r3.indexOf(null) + 6);
+    return r3.indexOf(null) + 6;
   }
   return undefined;
 }
@@ -92,7 +90,7 @@ function machine() {
     do {
       if (level == 0) {
         m = Math.floor(Math.random() * 9);
-      } else if(level == 1){
+      } else if (level == 1) {
         m = playL1(m);
       }
     } while (aval[m] != null);
@@ -110,14 +108,17 @@ function machine() {
 function check() {
   finish = win('o') || win('x');
   if (win('o')) {
-    message = 'User Win';
+    message = 'You Win 🎉';
+    wins++;
   } else
     if (win('x')) {
-      message = 'Machine Win';
+      message = 'Machine Win 🎃';
+      loses++;
     } else
       if (count == aval.length) {
         finish = true;
-        message = "Draw";
+        message = "Tie 🎭";
+        draws++;
       }
   if (finish) {
     if (confirm(message + "\nPlay again?")) {
@@ -126,8 +127,15 @@ function check() {
     let again = document.createElement('a');
     again.href = "/";
     again.innerHTML = "Play again";
+    again.className = "absolute right-0 top-0 p-3 bg-cyan-500";
     document.querySelector('body').appendChild(again);
   }
+  localStorage.wins = wins;
+  localStorage.loses = loses;
+  localStorage.draws = draws;
+  document.getElementById('win').innerHTML = wins;
+  document.getElementById('lose').innerHTML = loses;
+  document.getElementById('draw').innerHTML = draws;
 }
 
 function win(played) {
